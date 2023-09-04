88dd102feac666ff3fc2df554edc4b29749eb9b1
ayushmaangupta261 <132862778+ayushmaangupta261@users.noreply.github.com>
2023-09-04 13:25:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ function getFromSessionStorage() {
     if (!localCoordinates) {
         // agar local coorinates nhi mile iska matlab locationn ka acces nhi  diya gaya hai
         loadingScreen.classList.remove("active");
-        grantAccessContainer.classList.add("active");
-        //grantAccessButton.addEventListener("cli//ck", getLocation());
+      //  grantAccessContainer.classList.add("active");
+        //grantAccessButton.addEventListener("click", getLocation());
          getLocation();
     }
     else {
